279f23fef240e3d6d7b288469287577e278e3e70
aaronmichael726 <aaronmichael726@gmail.com>
2021-12-01 16:33:46 -0600

    moved random function to separate js file to avoid component mishaps see in other branches

--- src/components/board.tsx
+++ src/components/board.tsx
@@ -1,32 +1,25 @@
 import React from 'react'
 import './board.css'
 import Tile from './tile'
+import generateTokenArray from '../generateTokens'
 
-// horizontal and vertical axis consts for testing
-// const verticalAxis = [1,2,3,4,5,6,7,8]
-// const horizontalAxis = ['a','b','c','d','e','f','g','h']
-
-/**
- * Randomize between 1 and 3
- */
-
- function getRandomValue(max: number) {
-    return Math.floor(Math.random() * max)
-}
+let tokenArray = generateTokenArray
 
 export default function Board(){
     let board = []
+    let idx = 1
     
 
-    for(let j = 0; j < 8; j++){
-        for(let i = 0;  i < 8; i++){
+    for(let i = 0; i < 8; i++){
+        for(let j = 0;  j < 8; j++){
 
             const gridValue = j+i + 2
-            const tokenValue = getRandomValue(3)
             
-            board.push(<Tile number={gridValue} tokenValue={tokenValue}/>)
+            board.push(<Tile number={gridValue} tokenValue={tokenArray[i][j]} idx={idx} />)
+            idx++
         }
     }
 
     return <div id="board">{board}</div>
+    
 }
--- src/components/tile.tsx
+++ src/components/tile.tsx
@@ -3,20 +3,16 @@ import Token from './token'
 
 interface Props {
     number: number,
-    tokenValue: number,
+    tokenValue: string,
+    idx: number
 }
 
-/**
- * 
- * Need to generate png based on tokenValue 
- */
-
-export default function Tile({ number, tokenValue }: Props){
+export default function Tile({ number, tokenValue, idx }: Props){
 
     if (number % 2 === 0){
-        return <div className="tile dark-tile"><Token tokenValue={tokenValue} /></div>
+        return <div className="tile dark-tile"><Token tokenValue={tokenValue} idx={idx}/></div>
     } else {
-        return <div className="tile light-tile black-token"><Token tokenValue={tokenValue} /></div>
+        return <div className="tile light-tile black-token"><Token tokenValue={tokenValue} idx={idx}/></div>
     }
     
 }
