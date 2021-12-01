471364d4536bcb264b49f8b191e5c0b2e64c08df
aaronmichael726 <aaronmichael726@gmail.com>
2021-12-01 13:36:00 -0600

    created a new array for token value. logic will act on token array and not the rendered board.

--- src/components/board.tsx
+++ src/components/board.tsx
@@ -16,17 +16,26 @@ import Tile from './tile'
 
 export default function Board(){
     let board = []
-    
+    const tokenArray = new Array()
 
+    // I know there are 64 spaces, making this array 64 units for testing rn
+    for (let i = 0; i <= 63; i++){
+        tokenArray.push(getRandomValue(3))
+    }
+
+    let idx = 0
+
+    console.log('first run through ', tokenArray)
     for(let j = 0; j < 8; j++){
         for(let i = 0;  i < 8; i++){
 
             const gridValue = j+i + 2
-            const tokenValue = getRandomValue(3)
-            
-            board.push(<Tile number={gridValue} tokenValue={tokenValue}/>)
+            board.push(<Tile number={gridValue} tokenValue={tokenArray[idx]}/>)
+            idx++
         }
     }
+
+    console.log('second run through ', tokenArray)
 
     return <div id="board">{board}</div>
 }
