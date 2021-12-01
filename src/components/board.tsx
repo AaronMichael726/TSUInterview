6dc65d2d1f9a8ce0fe67bc29b27fda226f807418
aaronmichael726 <aaronmichael726@gmail.com>
2021-12-01 15:27:55 -0600

    this was not a good idea.

--- src/components/board.tsx
+++ src/components/board.tsx
@@ -2,23 +2,52 @@ import React from 'react'
 import './board.css'
 import Tile from './tile'
 
-// horizontal and vertical axis consts for testing
-// const verticalAxis = [1,2,3,4,5,6,7,8]
-// const horizontalAxis = ['a','b','c','d','e','f','g','h']
-
-/**
- * Randomize between 1 and 3
- */
-
- function getRandomValue(max: number) {
+// get num between 0, 1, 2
+function getRandomValue(max: number) {
     return Math.floor(Math.random() * max)
 }
 
-export default function Board(){
-    let board = []
-    let tokenArray = new Array()
+/**
+ * We check right to left, bottom to top, start there
+ */
+const isHorizontal = (board: any[][]) => {
+    let isMatch = 0
+    let totalMatch = 0
 
-    // I know there are 64 spaces, making this array 64 units for testing rn
+    console.log('from function', board)
+
+    for (let i = 7; i > 0; i--){
+        // checkVertical(board, i)
+        for (let j = 7; j > 0; j--){
+            // console.log('i: ', i, ' j: ', j)
+            // console.log('i: ', i, ' j: ', j-1)
+            // console.log('board[i][j]: ', board[i][j], 'board[i][j-1]:', board[i][j-1])
+
+            if (board[i][j] === board[i][j-1]){ 
+                isMatch++ 
+                console.log('duplicates: ', i, j)
+            }
+            if (isMatch === 3){
+                totalMatch++
+                isMatch = 0
+                console.log('isMatch on ', i, j)
+                console.log('total Mathces: ', totalMatch)
+            }
+
+            isMatch = 0
+        }
+    }
+
+    return totalMatch
+}
+
+// const checkVertical = (board: any[][], idx: number) => {
+
+// }
+
+function getTokenValue(){
+    let tokenArray = []
+
     for (let i = 0; i < 8; i++){
         tokenArray[i] = new Array()
         for (let j = 0; j < 8; j++){
@@ -26,32 +55,35 @@ export default function Board(){
         }   
     }
 
+    return tokenArray
+}
+
+
+export default function Board(){
+    let board = []
+    let idx=0
+
+    const tokenArray = getTokenValue()
+
+    let newArray = []
+
     for(let i = 0; i < 8; i++){
+        
+        newArray[i] = new Array()
         for(let j = 0;  j < 8; j++){
             const gridValue = j+i + 2
-            board.push(<Tile number={gridValue} tokenValue={tokenArray[i][j]}/>)
+            let tokenValue = tokenArray[i][j]
+            board.push(<Tile number={gridValue} tokenValue={tokenValue} idx={idx}/>)
+            newArray[i].push(tokenValue)
+
+            idx++ 
         }
     }
 
-    // test that all values in tokenArray are equal to the token value
-    // test confirms that array === tokenValue we can safely run logic on array
-    // console.log(tokenArray[0][0])
-    // console.log(board[0].props.tokenValue)
+    console.log(tokenArray)
+    console.log(newArray)
 
-    // let isTrue = 0
-    // let idx = 0
-    // for (let i=0; i < 8; i++){
-    //     for (let j = 0; j < 8; j++){
-    //         console.log(typeof tokenArray[i][j])
-    //         console.log(typeof board[idx].props.tokenValue)
-    //         if (tokenArray[i][j] === board[idx].props.tokenValue){
-    //             isTrue++
-    //         }
-    //         idx++
-    //     }
-
-    //     console.log(isTrue)
-    // }
+    
 
     return <div id="board">{board}</div>
 }
--- src/components/tile.tsx
+++ src/components/tile.tsx
@@ -4,6 +4,7 @@ import Token from './token'
 interface Props {
     number: number,
     tokenValue: number,
+    idx: number,
 }
 
 /**
@@ -11,7 +12,9 @@ interface Props {
  * Need to generate png based on tokenValue 
  */
 
-export default function Tile({ number, tokenValue }: Props){
+export default function Tile({ number, tokenValue, idx }: Props){
+
+    console.log('output: ', tokenValue)
 
     if (number % 2 === 0){
         return <div className="tile dark-tile"><Token tokenValue={tokenValue} /></div>
